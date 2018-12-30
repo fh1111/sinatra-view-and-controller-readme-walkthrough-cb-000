6c995a8e661050ace95eeb7601a5c06ed3a94125
Fer Hernandez <fh.1111@gmail.com>
2018-12-30 21:31:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,9 +17,7 @@ class App < Sinatra::Base
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
     erb :friends
 
-    <% @friends.each do |friend| %>
-        <h2><%= friend %></h2>
-    <% end %>
-    
+
+
   end
 end
